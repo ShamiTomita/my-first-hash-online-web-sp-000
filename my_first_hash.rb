1781edcf1f662d355d25b75dbebaacb7f438b8f8
Shami Tomita <shamitomita@gmail.com>
2021-01-13 22:59:58 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,7 +8,7 @@ def shipping_manifest
   the_manifest ={
                 "whale bone corsets" => 5,
                 "porecelain vases" => 2 
-                "oil painting" => 3
+                "oil paintings" => 3
   }
 end
 
